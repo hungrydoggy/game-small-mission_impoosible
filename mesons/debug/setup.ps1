bf70547a290de1717ca871eeca353b7352d06474
John Kim <jhoney510@gmail.com>
2025-12-05 23:25:51 +0900

    .

--- mesons/debug/setup.ps1
+++ mesons/debug/setup.ps1
@@ -1,2 +1,2 @@
 Set-Item -Path Env:PKG_CONFIG_PATH -Value "C:/vcpkg/installed/x64-windows/debug/lib/pkgconfig;$(Get-Item -Path Env:PKG_CONFIG_PATH).Value"
-meson setup ../../build/debug --backend=vs2022
+meson setup ../../build/debug --backend=ninja
